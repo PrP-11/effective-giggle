1eebc56febe75277e4451f2e170b65ff2381a64f
PRP <prp9812@gmail.com>
2019-04-25 00:12:31 +0530

    remove config file from gitignore

---
+++ config/connectVars.js
@@ -0,0 +1,8 @@
+const sqlVars = {
+    user: 'dummyUser',
+    password: 'dummyUser01',
+    host: 'db-intern.ciupl0p5utwk.us-east-1.rds.amazonaws.com',
+    database: 'db_intern'
+};
+
+module.exports = sqlVars;
